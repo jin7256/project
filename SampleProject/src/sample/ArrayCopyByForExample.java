ce0df202d9fd9bec220fa12211fd31386ebc4800
jinkyungpark <kyung5896@gmail.com>
2017-12-02 19:22:09 +0900

    배열복사 효율적인 방법 추가

--- SampleProject/src/sample/ArrayCopyByForExample.java
+++ SampleProject/src/sample/ArrayCopyByForExample.java
@@ -1,5 +1,7 @@
 package sample;
 
+import java.util.Arrays;
+
 public class ArrayCopyByForExample {
 	public static void main(String[] args) {
 		int[] oldIntArray = { 1, 2, 3 };
@@ -11,6 +13,12 @@ public class ArrayCopyByForExample {
 		
 		for(int i=0; i<newIntArray.length; i++) {
 			System.out.print(newIntArray[i] + ", ");
+		}
+		
+		//배열복사 효율적인 방법
+		int[] newArr=Arrays.copyOf(oldIntArray,oldIntArray.length);
+		for(int i:newArr) {
+			System.out.print(i+" ");
 		}
 	}
 }
